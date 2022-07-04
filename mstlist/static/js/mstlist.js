462817a51b9b979104f4004fe02041392248a858
Sakai <sakai@eel-corp.jp>
2022-07-04 15:42:53 +0900

    a

---
+++ mstlist/static/js/mstlist.js
@@ -0,0 +1,19 @@
+//サイドバーの開閉処理 
+$(function(){
+    $('#js-sidebar').click(function() {
+        $('.ui.sidebar').sidebar('toggle');
+    });
+})
+
+//削除ボタン メッセージ
+$('input:visible').first().focus();
+$(function(){
+  $(".btn_remove").click(function(){
+        if(confirm("削除しますか？")){
+            //yesの処理（何もぜず進む）
+        }else{
+            //cancelの処理
+            return false;
+        }
+    });
+})
